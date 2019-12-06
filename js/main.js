58a6ab34898ff07e311fd40c2796b79d93510bf6
unknown <mima3079@colorado.edu>
2019-12-06 00:22:22 -0700

    Motor Control modifications

--- js/main.js
+++ js/main.js
@@ -61,6 +61,107 @@ connectButton.addEventListener('click', () => {
       });
 });
 
+is_mouse_down = false;
+start_x = 0;
+start_y = 0;
+
+create_motorcommand = function(left, right){
+
+  if(left > 255){
+    left = 255;
+  }
+  if(left < -255){
+    left = -255;
+  }
+
+  if(right > 255){
+    right = 255;
+  }
+  if(right < -255){
+    right = -255;
+  }
+
+  motor_command = "M";
+
+  if(left < 0){
+    motor_command += "-";
+    left = -1 * left;
+  } else {
+    motor_command += "+";
+  }
+
+  if(left < 10){
+    motor_command += "00";
+  } else if(left < 100){
+    motor_command += "0";
+  }
+
+  motor_command += left;
+
+  motor_command += ",";
+
+  if(right < 0){
+    motor_command += "-";
+    right = -1 * right;
+  } else {
+    motor_command += "+";
+  }
+
+  if(right < 10){
+    motor_command += "00";
+  } else if(right < 100){
+    motor_command += "0";
+  }
+
+  motor_command += right;
+
+  return motor_command;
+
+}
+
+terminalContainer.addEventListener('mousedown', (e) => {
+
+  is_mouse_down = true;
+  start_x = e.screenX;
+  start_y = e.screenY;
+
+  return;
+
+});
+
+terminalContainer.addEventListener('mouseup', () => {
+
+  is_mouse_down = false;
+
+  motor_command = create_motorcommand(0, 0);
+  send(motor_command);
+
+  return;
+
+});
+
+terminalContainer.addEventListener('mousemove', (e) => {
+
+  if(is_mouse_down){
+
+    dx = start_x - e.screenX;
+    dy = start_y - e.screenY;
+
+    left = Math.round( dy - ( dx / 10 ) );
+    right = Math.round( dy + ( dx / 10 ) );
+
+    motor_command = create_motorcommand(left, right);
+
+    //console.log(motor_command);
+
+    send(motor_command);
+
+  }
+
+  return;
+
+});
+
 disconnectButton.addEventListener('click', () => {
   terminal.disconnect();
   deviceNameLabel.textContent = defaultDeviceName;
